0462cc98c32d1897933fe91eaab8a3b6a176edc6
Sergii Taran <serg.taran1970@gmail.com>
2026-04-26 17:45:31 +0300

    add middleware with ES modules: cors, express.json, pino-http

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
