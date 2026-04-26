d9aed51526aa50c2bb3f4cfc5b0897f50a00d7b5
Sergii Taran <serg.taran1970@gmail.com>
2026-04-26 17:26:49 +0300

    add GET /notes and GET /notes/:noteId routes

--- src/server.js
+++ src/server.js
@@ -1,39 +1,24 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
 
-// Логування часу
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
+const PORT = process.env.PORT || 3000;
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
 });
 
-// Кореневий маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!' });
-});
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
 
-// Маршрут для тестування middleware помилки
-app.get('/test-error', (req, res) => {
-  // Штучна помилка для прикладу
-  throw new Error('Something went wrong');
-});
-
-// Middleware 404 (після всіх маршрутів)
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// Middleware для обробки помилок (останнє)
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: 'Internal Server Error',
-    error: err.message,
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
