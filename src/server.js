54b606b6521d5b77f01a02cd7b2cfd5e3ab11c76
Sergii Taran <serg.taran1970@gmail.com>
2026-04-26 22:06:13 +0300

    add global error handler and test error route

--- src/server.js
+++ src/server.js
@@ -25,8 +25,20 @@ app.get('/notes/:noteId', (req, res) => {
   });
 });
 
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+
+  res.status(500).json({
+    message: err.message,
+  });
 });
 
 app.listen(PORT, () => {
