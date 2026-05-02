8bbceecccbaa86b0e0997250aee1f57e2aff17aa
Sergii Taran <serg.taran1970@gmail.com>
2026-05-02 20:32:15 +0300

    feat: implement GET /notes/:noteId endpoint

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res, next) => {
@@ -5,6 +6,22 @@ export const getAllNotes = async (req, res, next) => {
     const notes = await Note.find();
 
     res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getNoteById = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json(note);
   } catch (error) {
     next(error);
   }
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllNotes } from '../controllers/notesController.js';
+import { getAllNotes, getNoteById } from '../controllers/notesController.js';
 
 const router = express.Router();
 
 router.get('/notes', getAllNotes);
+router.get('/notes/:noteId', getNoteById);
 
 export default router;
