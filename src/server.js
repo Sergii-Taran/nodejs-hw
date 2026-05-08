2ec240aa0ee601576450dd233429c74c1b7c874b
Sergii Taran <serg.taran1970@gmail.com>
2026-05-08 07:30:11 +0300

    add celebrate validation for notes routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { errors } from 'celebrate';
 
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use(logger);
 
 app.use(notesRoutes);
+
+app.use(errors());
 
 app.use(notFoundHandler);
 app.use(errorHandler);
