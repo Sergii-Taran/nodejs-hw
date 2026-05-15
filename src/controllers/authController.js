469a59f2b37a349e1b648521311df8c4c2ae2811
Sergii Taran <serg.taran1970@gmail.com>
2026-05-15 22:28:39 +0300

    add user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,8 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 
+import { Session } from '../models/session.js';
+
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
 
@@ -25,4 +27,28 @@ export const registerUser = async (req, res) => {
   setSessionCookies(res, session);
 
   res.status(201).json(user);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const session = await createSession(user._id);
+
+  setSessionCookies(res, session);
+
+  res.status(200).json(user);
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 
-import { registerUser } from '../controllers/authController.js';
-import { registerUserSchema } from '../validations/authValidation.js';
+import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+} from '../validations/authValidation.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', celebrate(registerUserSchema), registerUser);
+authRouter.post('/login', celebrate(loginUserSchema), loginUser);
 
 export default authRouter;
