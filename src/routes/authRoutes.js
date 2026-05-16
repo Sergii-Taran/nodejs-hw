401f0e632edd915d1278b378b1034f0a1d362bed
Sergii Taran <serg.taran1970@gmail.com>
2026-05-16 06:21:18 +0300

    add session refresh

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 
-import { registerUser, loginUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+} from '../controllers/authController.js';
 import {
   registerUserSchema,
   loginUserSchema,
@@ -11,5 +15,6 @@ const authRouter = Router();
 
 authRouter.post('/register', celebrate(registerUserSchema), registerUser);
 authRouter.post('/login', celebrate(loginUserSchema), loginUser);
+authRouter.post('/refresh', refreshUserSession);
 
 export default authRouter;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -18,6 +19,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
 app.use(logger);
 
 app.use(notesRoutes);
